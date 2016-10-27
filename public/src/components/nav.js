a01e35b6c92d392ad5c9b1443ca6abdb25399d2f
RichHong <rich.hong@gmail.com>
2016-10-26 20:27:57 -0700

    change plus sign

--- public/src/components/nav.js
+++ public/src/components/nav.js
@@ -29,7 +29,7 @@ class Nav extends Component {
             <Link to='/'><i className="fa fa-home fa-3x tooltip" aria-hidden="true"><span className="tooltiptext">Home</span></i></Link>
           </li>
           <li>
-            <Link to='/addQuiz'><i className="fa fa-plus-square fa-3x tooltip" aria-hidden="true"><span className="tooltiptext">Add Quiz</span></i></Link>
+            <Link to='/addQuiz'><i className="fa fa-plus fa-3x tooltip" aria-hidden="true"><span className="tooltiptext">Add Quiz</span></i></Link>
           </li>
           <li>
             <div onClick={e => this.saveLocal(e)}><i className="fa fa-floppy-o fa-3x tooltip" aria-hidden="true"><span className="tooltiptext">Save Quizzes</span></i></div>
