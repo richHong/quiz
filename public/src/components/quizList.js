b8abab7e524ba1e864750db0c3b3e47bd24385e9
RichHong <rich.hong@gmail.com>
2016-11-10 01:05:34 -0500

    changed loading spinner src

--- public/src/components/quizList.js
+++ public/src/components/quizList.js
@@ -7,7 +7,7 @@ class QuizList extends Component {
     return (
       <div>
         {!localQuizzes && !this.props.quizzes.length ? null : 
-          localQuizzes && !this.props.quizzes.length ? <img className='loader' src='https://www.robtaber.com.au/img/ajax-loader.gif'/> : 
+          localQuizzes && !this.props.quizzes.length ? <img className='loader' src='http://blog.teamtreehouse.com/wp-content/uploads/2015/05/InternetSlowdown_Day.gif'/> : 
           this.props.quizzes.map( (quiz, i) => {
           return (
             <QuizItem key={i} quiz={quiz} />
